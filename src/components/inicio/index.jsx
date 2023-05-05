d8c7c790188068dbb53061f8360540ce6c5abe92
PEDRO ESTEBAN <pesteban@andreani.com>
2023-05-05 13:19:40 -0300

    feat: nueva sección registros

--- src/components/inicio/index.jsx
+++ src/components/inicio/index.jsx
@@ -1,21 +1,43 @@
 import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { addUser } from '../../redux/userSlice';
 import './index.css'
 
 
 const Body = () => {
-  const [showModal, setShowModal] = useState(false);
+
+  const dispatch = useDispatch()
+  const [showModal, setShowModal] = useState(false)
+  const [registro, SetRegistroData] = useState({
+    name: '',
+    email: '',
+    priority: '',
+    status: 'no iniciado',
+    task: '',
+  })
+
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    SetRegistroData({ ...registro, [name]: value })
+  }
+
 
   const handleShowModal = () => {
-    setShowModal(true);
+    setShowModal(true)
   }
 
   const handleCloseModal = () => {
-    setShowModal(false);
+    setShowModal(false)
   }
 
-  // const handleSave = (event) => {
+  const handleSave = () => {
+    if (registro.name !== '' && registro.email !== '') {
+      setShowModal(false)
+      dispatch(addUser)
+      SetRegistroData({ name: '', email: '', priority: '', status: 'no iniciado', task: '', })
+    }
+  }
 
-  // }
   return (
     <React.Fragment>
       <button className='newTask' onClick={handleShowModal}>
@@ -27,40 +49,59 @@ const Body = () => {
           <div className='modal'>
             <h1 id='modalH1'>Registro</h1>
             <label id='name'>Name:</label>
-            <input type='text' id='name' name='name' />
+            <input type='text' id='name' name='name'
+              value={registro.name} onChange={handleChange} />
 
             <label id='email'>Email:</label>
-            <input type='email' id='email' name='email' />
+            <input type='email' id='email' name='email' value={registro.email} onChange={handleChange} />
 
             <label id='priority'>Priority:</label>
             <select id='priority' name='priority'>
-              <option value='alto'>Alto</option>
-              <option value='medio'>Medio</option>
-              <option value='bajo'>Bajo</option>
+              <option value={registro.priority = 'alto'} onChange={handleChange}>Alto</option>
+              <option value={registro.priority = 'medio'} onChange={handleChange}>Medio</option>
+              <option value={registro.priority = 'bajo'} onChange={handleChange}>Bajo</option>
             </select>
 
             <label id='status'>Status:</label>
             <select id='status' name='status'>
-              <option value='alto'>No iniciado</option>
-              <option value='medio'>Iniciado</option>
+              <option value='alto' onChange={handleChange}>No iniciado</option>
+              <option value='medio' onChange={handleChange}>Iniciado</option>
             </select>
 
             <label id='task'>Assigned task:</label>
-            <textarea id='task' name='task' rows='4'></textarea>
+            <textarea id='task' name='task' rows='4' value={registro.task} onChange={handleChange}></textarea>
 
             <div className='modal-buttons'>
-              <button id='saveButton'>Guardar</button>
+              <button id='saveButton' onClick={handleSave}>Guardar</button>
               <button id='cancelButton' onClick={handleCloseModal}>Cancel</button>
             </div>
           </div>
         </div>
       )}
 
-      <div className='noTasks'>
-        There are no registered tasks
-      </div>
+      {registro.name != 0 ? (
+        <div className='tasks'>
+          <div>
+            {registro.name}
+          </div>
+          <div>
+            {registro.email}
+          </div>
+          <div>
+            {registro.priority}
+          </div>
+          <div>
+            {registro.task}
+          </div>
+        </div>
+
+      ) : (
+        <div className='noTasks'>
+          There are no registered tasks
+        </div>
+      )}
     </React.Fragment>
-  );
-};
+  )
+}
 
 export default Body
---
+++ src/redux/userSlice.js
@@ -0,0 +1,31 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    name: '',
+    email: '',
+    status: '',
+    priority: '',
+    status: 'no iniciado'
+}
+
+export const userSlice = createSlice({
+    name: 'user',
+    initialState,
+    reducer: {
+        addUser: (state, action) => {
+            const { name, email, priority, status } = action.payload
+            state.name = name
+            state.email = email
+            state.priority = priority
+            state.status = status
+        },
+        changeStatus: (state, action) => {
+            state.status = action.payload
+        }
+    }
+})
+
+
+
+export const { addUser, changeStatus } = userSlice.actions
+export default userSlice.reducer
